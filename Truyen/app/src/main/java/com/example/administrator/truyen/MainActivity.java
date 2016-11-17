2de986f6000bfeda0651cb466c5394d94e285bfb
HMCGHOS-EJO11JS\Administrator <vinhquang1710@gmail.com>
2016-11-17 14:06:41 +0700

    no message

---
+++ Truyen/app/src/main/java/com/example/administrator/truyen/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.truyen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
